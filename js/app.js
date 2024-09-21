557db1014199f6a882421d49cb2f27279e1dc108
istoriya-islama <istoriyaislama@hotmail.com>
2024-09-22 01:22:42 +0300

    Not PWA app Part 2

--- js/app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('js/sw.js')
-    .then(() => console.log('service worker registered'))
-    .catch(() => console.log('service worker not registered'));
-}
